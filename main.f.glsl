ef83fb004ff35bc64f7b8688741775c139ed13b4
labrat97 <james@nineseven.net>
2022-01-07 08:10:14 -0800

    Added comments, `treeavg()`, and an inner bias

--- main.f.glsl
+++ main.f.glsl
@@ -3,17 +3,33 @@ precision highp float;
 
 #define PI 3.141592653589
 #define PHI 1.618033988749894848204586834
-#define DIVIDER PI
+#define DIVIDER (3./2.) * PI
+#define INNER_BIAS 1.
 
+// Pull in program data
 uniform vec2 winsize;
 uniform float minwid;
 uniform int now;
 
+// Add all of the cubes together, perform a cube root, then multiply x to the result.
+float treeavg(float x, float y, float z) {
+    float x3 = pow(x, 3.);
+    float y3 = pow(y, 3.);
+    float z3 = pow(z, 3.);
+
+    return x * pow(x3 + y3 + z3, 1./3.);
+}
+
 void main() {
-    // Grab the center of the canvas according to the shortest side
-    vec2 center = ((2.0 * (gl_FragCoord.xy / winsize)) - 1.0) 
+    // Grab the center of the canvas according to the shortest side. Put into an
+    // approximate range of [0., 1.].
+    vec2 center = ((2.0 * (gl_FragCoord.xy / winsize)) - INNER_BIAS) 
         * ((winsize * DIVIDER) / minwid);
-    float mag = sqrt(pow(center.x, 2.) + pow(center.y, 2.));
+
+    // Get the distance from the center in the function. Due to the way that the
+    // log() function is implemented, simply subtracting the bias of the pupil
+    // accounds for the required floor function.
+    float mag = sqrt(pow(center.x, 2.) + pow(center.y, 2.)) - 1.;
 
     // Add the date to the angle for some sort of motion, slow, but motion.
     float angle = atan(center.y, center.x) + (2. * PI * mod(float(now)/196883., 1.));
@@ -35,9 +51,9 @@ void main() {
     float g = (-sin((2.*PI/3.)-angle) + 1.) / 2.;
 
     // Logrithmically scale values to make blend work more naturally.
-    r = log2(r+1.);
-    b = log2(b+1.);
-    g = log2(g+1.);
+    r = treeavg(r, g, b);
+    b = treeavg(b, g, r);
+    g = treeavg(g, r, b);
 
     // Apply the color and intensity to the display
     vec3 precolor = vec3(r, g, b);
